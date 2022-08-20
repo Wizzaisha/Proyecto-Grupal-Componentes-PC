b57426c0ffe724acf241b75514c494e866c4cd78
Wizzaisha <ever.ramirez@usantotomas.edu.co>
2022-08-20 13:16:49 -0500

    boostrap scripts añadidos y algunos ajustes a botones

--- client/src/components/CategoriesBar/index.jsx
+++ client/src/components/CategoriesBar/index.jsx
@@ -11,11 +11,14 @@ import {
 function CategoriesBar (props) {
 
     const allCategories = useSelector(state => state.allCategories);
+    const currentCategory = useSelector(state => state.category);
 
     const dispatch = useDispatch();
 
     // By category
-    function handleCategorySelect(value) {
+    function handleCategorySelect(event) {
+
+        const { value } = event.target;
 
         dispatch(setCategory(value))
         dispatch(filterAndSortBy());
@@ -27,20 +30,42 @@ function CategoriesBar (props) {
 
     return (
         <div>
-            <p>Categories</p>
-            <div className="list-group list-group-horizontal">
-                {allCategories && allCategories.map((category, index) => {
-                    return (
-                        <button 
-                            onClick={() => {
-                                handleCategorySelect(category.name);
-                            }}
-                            key={index}
-                            className={`list-group-item list-group-item-action`}
-                        >{category.name}</button>
-                    )
-                })}
+            <button 
+                className="btn btn-secondary" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#collapseExample" 
+                aria-expanded="false" 
+                aria-controls="collapseExample"
+            >Filter by categories
+            </button>
+            <div id="collapseExample" className="collapse">
+                <div className="btn-group-vertical">
+                    {allCategories && allCategories.map((category, index) => {
+                        return (
+                            <div key={index}>
+                                <label 
+                                    className={`radioButton btn btn-outline-secondary ${currentCategory === category.name ? "checked" : "nochecked"} `}
+                                >
+                                    <input
+                                        type={"radio"} 
+                                        className="btn-check"
+                                        autoComplete="off"
+                                        value={category.name}
+                                        onChange={handleCategorySelect}
+                                        checked={currentCategory === category.name ? true : false}
+                                    /> 
+                                    {category.name}
+                                </label>
+
+                            </div>
+
+                        )
+                    })}
+                </div>
             </div>
+            
+
         </div>
     )
 }
