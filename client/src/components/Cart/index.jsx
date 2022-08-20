098538329ae7d4812140fe373a9a78831008493a
FrancoLedi <francoivanledesma551@gmail.com>
2022-08-20 18:23:35 -0300

    ADD TO CART REMOVE FROM CART & CART COMPONENT

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React /*{ useState }*/ from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, removeFromCart } from "../../redux/actions";
 //import { Link } from "react-router-dom";
@@ -11,20 +11,20 @@ function Cart () {
 
     const dispatch = useDispatch()
 
-    const [input, setInput] = useState({
-        aux: 1
-    })
-    
+   
+    /*
+
+    BOTON PARA PROBAR AGREGAR UN PRODUCTO
+
     function handleButton(e) {
         e.preventDefault();
-        dispatch(addToCart(input.aux));
-        setInput({
-            aux: input.aux+1
-        })
-        
+        dispatch(addToCart(e.target.value));
     }
 
+    <button class={s.button} value={el.id} type='button' onClick={(e) => handleButton(e)}> Add a product </button>
+    */
     function handleDelete(e) {
+        // El handler este le da el ID a la accion del producto que quiere remover
         e.preventDefault();
         dispatch(removeFromCart(e.target.value));
         console.log(e.target.value)
@@ -38,8 +38,8 @@ function Cart () {
             <span class={s.span}>Price</span>
             <span class={s.span}>Product</span>
             </div>
-            <button class={s.button} type='button' onClick={(e) => handleButton(e)}> Add a product </button>
             {
+                // Mapeo el carrito que me traigo con useSelector para crear cada item
                 cart.map(el =>
                     
                     
@@ -57,7 +57,7 @@ function Cart () {
                             min = "0"                   // Aca me falta handelear las cantidades
                             max = "100" 
                         />
-                
+                        {/* Este boton que se renderiza por cada uno de los componentes toma como value el ID del producto */}
                         <button class={s.button} value={el.id} type='button' onClick={(e) => handleDelete(e)}> X </button>
                         
                     </div> )
@@ -70,6 +70,7 @@ function Cart () {
 export default Cart;
 
 /*
+ Ignorar esto
 | Propiedades del input Units |
 value = {input.units} 
                             name = 'units'
