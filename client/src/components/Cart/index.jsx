e9dfc1e680f41f753a6d5c5c08a5eabe74bf8382
Wizzaisha <ever.ramirez@usantotomas.edu.co>
2022-08-20 21:40:57 -0500

    otros ajustes a cart

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -28,34 +28,34 @@ function Cart () {
     }
 
     return (
-        <div class={"container"}>
+        <div className={"container"}>
             <h1>Cart</h1>
-            <div class={"headers"}>
-            <span class={"span"}>Units</span>
-            <span class={"span"}>Price</span>
-            <span class={"span"}>Product</span>
+            <div className={"headers"}>
+            <span className={"span"}>Units</span>
+            <span className={"span"}>Price</span>
+            <span className={"span"}>Product</span>
             </div>
             {
                 // Mapeo el carrito que me traigo con useSelector para crear cada item
                 cart.map(el =>
                     
                     
-                        <div class = {"items"}>
-                        <div class = {"product"}>
+                        <div className = {"items"}>
+                        <div className = {"product"}>
                         <img src={el.background_image} alt='noimage'></img>
-                        <p class={"span"}>{el.marca + ' ' + el.modelo}</p>
+                        <p className={"span"}>{el.marca + ' ' + el.modelo}</p>
                         </div>
         
-                        <label class={"span"}>{`$ ${el.precio}`}</label>
+                        <label className={"span"}>{`$ ${el.precio}`}</label>
 
-                        <input class={"span"}
+                        <input className={"span"}
                             id = {"units"}
                             type = "number"
                             min = "0"                   // Aca me falta handelear las cantidades
                             max = "100" 
                         />
                         {/* Este boton que se renderiza por cada uno de los componentes toma como value el ID del producto */}
-                        <button class={"button"} value={el.id} type='button' onClick={(e) => handleDelete(e)}> X </button>
+                        <button className={"button"} value={el.id} type='button' onClick={(e) => handleDelete(e)}> X </button>
                         
                     </div> )
             }
