e36d6efe8e7081e3577da919a9823fb809480527
Arrua Alejandro <94117716+power38261010@users.noreply.github.com>
2022-08-22 10:24:35 -0300

    a

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -2,10 +2,10 @@ const {Router} = require('express')
 const {  checkDuplicateUsernameOrEmail, verifySignUp , controller } = require("../Auth");
 const router = Router()
 
-router.post("/signup",
-   [checkDuplicateUsernameOrEmail, verifySignUp],
-    controller.signup
-  );
+// router.post("/signup",
+//    [checkDuplicateUsernameOrEmail, verifySignUp],
+//     controller.signup
+//   );
   
   router.post("/signin", controller.signin);
   
