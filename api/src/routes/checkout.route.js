e0224cb23cc359a9a6bb0808c1f2b6ce042d94c0
Wizzaisha <ever.ramirez@usantotomas.edu.co>
2022-08-24 00:23:34 -0500

    algunos cambios al pago

--- api/src/routes/checkout.route.js
+++ api/src/routes/checkout.route.js
@@ -3,6 +3,8 @@ const {Router} = require('express');
 const Stripe = require("stripe");
 const router = Router();
 
+const { Op } = require("sequelize");
+const { OrderProduct, User } = require("../db");
 
 const {
     STRIPE_S_KEY,
@@ -13,6 +15,7 @@ const stripe = new Stripe(STRIPE_S_KEY);
 const getAmount = (items) => {
     return items.map(e => e.price * e.quantity).reduce((curr, next) => curr + next, 0);
 }
+
 
 router.post("/", async (req, res, next) => {
     
@@ -25,20 +28,16 @@ router.post("/", async (req, res, next) => {
     } = req.body;
 
     try {
-
-        console.log(req.body);
-
-        const amount = getAmount(products) * 100;
         
         const payment = await stripe.paymentIntents.create({
             amount,
             currency: "usd",
-            description: "ordernumber: 123231",
             payment_method: method_pay,
             confirm: true,
-            receipt_email,
             shipping,
         });
+
+
 
         console.log(payment);
         res.status(201).send({message: "Successfull pay"});
