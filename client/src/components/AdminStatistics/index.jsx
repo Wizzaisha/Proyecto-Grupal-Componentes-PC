7e092ce30651f4dad55cf1036e35b9b6d29e59e7
Wizzaisha <ever.ramirez@usantotomas.edu.co>
2022-08-31 09:18:45 -0500

    graficos avance

--- client/src/components/AdminStatistics/index.jsx
+++ client/src/components/AdminStatistics/index.jsx
@@ -1,14 +1,43 @@
 import { useSelector } from "react-redux";
 import "./AdminStatistics.css";
 
+import {
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip
+  } from "recharts";
+
 
 function AdminStatistics () {
     
-    const orders = useSelector(state => state.orderList);
+    const statisticsData = useSelector(state => state.statisticsData);
 
+    console.log(statisticsData);
     return (
         <div>
-            <p>Statistics</p>
+            <div>
+                <p>Daily Sales</p>
+                <AreaChart 
+                    width={500}
+                    height={400}
+                    data={statisticsData.dailyData}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="day" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="totalAmount" stroke="#8884d8" fill="#8884d8" />
+                </AreaChart>
+            </div>
         </div>
     )
 }
