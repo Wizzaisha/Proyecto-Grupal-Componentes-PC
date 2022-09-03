3da0d8cdd1f2ac2b27fef19b5417f405ec190f2a
unknown <santi.cervan.12@gmail.com>
2022-09-03 15:00:00 -0300

    profile trae favorites por consola

--- api/src/routes/OrderList.route.js
+++ api/src/routes/OrderList.route.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const {Router} = require('express');
+const { Router } = require('express');
 const Stripe = require("stripe");
 const router = Router();
 
@@ -29,42 +29,42 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:idPayment", async (req, res, next) => {
-    
+
     const { idPayment } = req.params;
-    
+
     const response = await stripe.paymentIntents.retrieve(
         idPayment
     );
-    
+
     const paymentDetails = await oneDataController(response);
 
     res.status(200).send(paymentDetails);
 })
 
 //Get payments of a customer using the id
-router.get("/customer/:email", async (req, res, next) => {
+// router.get("/customer/:email", async (req, res, next) => {
 
-    const { email } = req.params;
+//     const { email } = req.params;
 
-    const findCustomer = await stripe.customers.search({
-        query: `email:\'${email}\'`
-    });
+//     const findCustomer = await stripe.customers.search({
+//         query: `email:\'${email}\'`
+//     });
 
-    const response = await stripe.paymentIntents.list({
-        customer: findCustomer.data[0].id,
-        limit: 100
-    });
+//     const response = await stripe.paymentIntents.list({
+//         customer: findCustomer.data[0].id,
+//         limit: 100
+//     });
 
 
-    const listPayments = await dataOrderControllerCustomer(response.data);
+//     const listPayments = await dataOrderControllerCustomer(response.data);
 
-    res.status(200).send(listPayments);
-});
+//     res.status(200).send(listPayments);
+// });
 
 router.post("/:idPayment", async (req, res, next) => {
     const { idPayment } = req.params;
     const typeUpdate = req.body;
-    
+
     const response = await stripe.paymentIntents.update(
         idPayment,
         typeUpdate
--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -3,13 +3,17 @@ import "./Profile.css";
 import { useNavigate, Outlet } from "react-router-dom";
 import { useAuth } from '../context/authContext';
 import { getCustomerHistory } from "../../redux/actions";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 
 
-function Profile(){
+function Profile() {
 
+    let productState = useSelector(state => state.products);
+    let allProducts = productState.filter(e => e.isDeleted === false)
+
+    const [favorite, setFavorite] = useState([])
     const auth = useAuth();
     const navigate = useNavigate();
     const dispatch = useDispatch();
@@ -18,10 +22,12 @@ function Profile(){
         navigate(`/profile/purchase-history/`);
     }
 
-    function handleFavoriteClick() {
+    const handleFavoriteClick = async () => {
         console.log("Omg a favoritos");
+        setFavorite(await auth.getFavorite())
+        console.log(favorite);
     }
-    
+
     useEffect(() => {
         dispatch(getCustomerHistory(auth.user.email));
     }, [dispatch, auth.user.email]);
@@ -45,7 +51,14 @@ function Profile(){
                 <button type="button" className="btn btn-primary" onClick={handleHistoryClick}>Purchase history</button>
                 <button type="button" className="btn btn-primary" onClick={handleFavoriteClick}>Favorites</button>
             </div>
-
+            <div>
+                {/* {favorite && favorite.map((e) => {
+                    let filter = allProducts.filter(f => f.id === e)
+                    return filter.map((p) => {
+                        return <div>{p}</div>
+                    })
+                })} */}
+            </div>
             <div>
                 <Outlet />
             </div>
