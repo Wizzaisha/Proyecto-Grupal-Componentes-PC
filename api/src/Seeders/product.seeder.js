0057582de73def42ae17c5352788ea6ae7d2b0ef
Wizzaisha <ever.ramirez@usantotomas.edu.co>
2022-09-06 18:59:08 -0500

    añadir algo para el control de los seeders

--- api/src/Seeders/product.seeder.js
+++ api/src/Seeders/product.seeder.js
@@ -1,11 +1,20 @@
 const { PRODUCTOS } = require("./productos")
 const { crearProducto } = require("../Middleware/createProduct.middleware")
+const { Product } = require("../db");
 
-const productoSeeder = () => {
-    PRODUCTOS.map((e) => {
-        crearProducto(e.brand, e.model, e.image, e.description, e.specs, e.benchmark, e.price, e.stock, e.category)
+const productoSeeder = async () => {
 
-    })
-    console.log("SeederProduct")
+    const response = await Product.findAll();
+
+    if (response.length > 0){
+        console.log("Los Productos ya estan cargados");
+    } else {
+        PRODUCTOS.map((e) => {
+            crearProducto(e.brand, e.model, e.image, e.description, e.specs, e.benchmark, e.price, e.stock, e.category)
+    
+        })
+        console.log("SeederProduct")
+    }
+
 }
 module.exports = { productoSeeder }
