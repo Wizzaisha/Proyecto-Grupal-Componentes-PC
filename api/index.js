952650d25bcaa2be6f3a522e79f203c46a67f9b4
unknown <santi.cervan.12@gmail.com>
2022-09-07 08:59:56 -0300

    lol

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const {seederCategory} = require("./src/Seeders/category.seeder")
-const { seederRole} = require("./src/Seeders/role.seeder")
-const {productoSeeder} = require("./src/Seeders/product.seeder")
+const { seederCategory } = require("./src/Seeders/category.seeder")
+const { seederRole } = require("./src/Seeders/role.seeder")
+const { productoSeeder } = require("./src/Seeders/product.seeder")
 
 conn.sync().then(() => {
 
