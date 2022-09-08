b72eab9d17169449a0b4e168ea896dad9587f8ea
unknown <santi.cervan.12@gmail.com>
2022-09-07 21:57:30 -0300

    ya funcionan las respuestas desde admin

--- client/src/components/AdminAnswers/index.jsx
+++ client/src/components/AdminAnswers/index.jsx
@@ -1,33 +1,82 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import axios from "axios";
+import { getAllProducts, responseQuestion } from "../../redux/actions";
+
 
 const AdminAnswers = () => {
     const dispatch = useDispatch()
     const productos = useSelector(state => state.productsCopy)
+    const [answer, setAnswer] = useState({
+        sendAdmin: "",
+        emailAdmin: "",
+        id: 0
+    })
+    const [productQuestions, setProductQuestions] = useState([])
 
     useEffect(() => {
-
+        dispatch(getAllProducts())
     }, [])
 
+    useEffect(() => {
+        setProductQuestions(productos.filter((e) => e.questions.length > 0))
+    }, [])
+
+    useEffect(() => {
+        setProductQuestions(productos.filter((e) => e.questions.length > 0))
+    }, [productos])
+
+    const handleInputChange = (e) => {
+        setAnswer({
+            sendAdmin: e.target.value,
+            emailAdmin: e.target.value
+        })
+    }
+
+    const handleAnswer = (ev, id) => {
+        ev.preventDefault()
+        if (answer.sendAdmin.length > 0 && answer.emailAdmin.length > 0) {
+            dispatch(responseQuestion(parseInt(id), answer))
+            dispatch(getAllProducts())
+            console.log(id);
+        }
+    }
+
+    // const productQuestions = productos.filter((e) => e.questions.length > 0)
+    console.log(productQuestions);
     return (
         <div>
             <h1>Hola mundo</h1>
-            {productos && productos.map(e => {
-                const getAnswers = async () => {
-                    const respuesta = await axios.get(`http://localhost:3001/api/question/${e.id}`)
-                    return respuesta.data
-                }
-                const question = getAnswers()
-                // if (question && question.length > 0) {
-                //     return (
-                //         <div>
-                //             <h2>{e.model}</h2>
-                //         </div>
-                //     )
-                // }
-                console.log(question);
-            })}
+            {productQuestions && productQuestions.length > 0 ? (
+                <div>
+                    {productQuestions.map(e => {
+                        if (e.questions.some(que => que.sendAdmin === null)) {
+                            return (
+                                <div>
+                                    <img src={e.image} className="w-25" alt='img' />
+                                    <h3>{`${e.brand} ${e.model}`}</h3>
+                                    {e.questions?.filter(e => e.sendAdmin === null).map(e => (
+                                        <div>
+                                            <h3>{e.emailUser}</h3>
+                                            <h4>{e.sendUser}</h4>
+
+                                            <form onSubmit={(ev) => { handleAnswer(ev, e.id) }}>
+                                                <input type="text" name="answer" onChange={(e) => handleInputChange(e)}></input>
+                                                <button type='submit'>Answer</button>
+                                            </form>
+
+                                        </div>
+                                    ))}
+                                </div>
+                            )
+                        }
+
+                    })}
+                </div>
+            ) : (
+                <div>
+                    <p>There are not questions yet...</p>
+                </div>
+            )}
         </div>
     )
 }
